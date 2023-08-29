86bbd65b3fb4ce6200dd361efd1733ca0081ebab
Julian Bustos <julian.m.bustos@gmail.com>
2023-08-29 12:44:59 -0400

    ads the schema for the descuentos

--- sanity/schemas/documents/descuentos.ts
+++ sanity/schemas/documents/descuentos.ts
@@ -10,5 +10,45 @@ export const descuentosSchema = defineType({
       title: "Título",
       type: "string",
     }),
+    defineField({
+      name: "porcentaje",
+      title: "Porcentaje",
+      type: "string",
+    }),
+    defineField({
+      name: "productos",
+      title: "Productos",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [
+            { type: "gafasLujo" },
+            { type: "gafasPremium" },
+            { type: "relojesLujo" },
+            { type: "relojesPremium" },
+            { type: "perfumeLujo" },
+            { type: "perfumePremium" },
+          ],
+        },
+      ],
+    }),
+    defineField({
+      name: "duracion",
+      title: "Duración",
+      type: "object",
+      fields: [
+        defineField({
+          name: "inicio",
+          title: "Inicio",
+          type: "date",
+        }),
+        defineField({
+          name: "fin",
+          title: "Fin",
+          type: "date",
+        }),
+      ],
+    })
   ],
 });
