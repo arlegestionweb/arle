ff9e5964bf89b8e7ea4445c80329f6c2df019baa
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-09-17 13:46:05 -0500

    Add banners to explore product sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getHomepageContent } from "@/sanity/queries/pages/homepageQuery";
 import Banner from "./_components/Banner";
+import ExploreSection from "./_components/ExploreSection";
 
 const Home = async function () {
   const pageContent = await getHomepageContent();
@@ -9,7 +10,11 @@ const Home = async function () {
 
   return (
     <main>
-      <Banner banners={pageContent.banners}></Banner>
+      <Banner banners={pageContent.banners}/>
+      {exploreSections.map((section, index) => (
+        <ExploreSection key={index} section={section} />
+      ))}
+      
     </main>
   );
 };
