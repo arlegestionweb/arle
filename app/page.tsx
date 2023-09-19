2d3ec78ace62a66d01c4f2a041622b96a83f4c51
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-09-18 22:31:33 -0500

    Resolve rebase conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
 import { getHomepageContent } from "@/sanity/queries/pages/homepageQuery";
-<<<<<<< HEAD
-=======
 import Banner from "./_components/Banner";
 import ExploreSection from "./_components/ExploreSection";
->>>>>>> ff9e596 (Add banners to explore product sections)
 
 const Home = async function () {
   const pageContent = await getHomepageContent();
-  // console.log(pageContent);
+  console.log(pageContent);
+  
+  const exploreSections = [pageContent.perfumes, pageContent.relojes, pageContent.gafas]
+
   return (
-<<<<<<< HEAD
-    <main className="min-h-screen bg-background grid place-content-center">
-      <h1 className="font-bold text-4xl">Hello Arlé world!!</h1>
-=======
     <main>
-      <Banner banners={pageContent.banners}/>
+      <Banner banners={pageContent.banners}></Banner>
       {exploreSections.map((section, index) => (
         <ExploreSection key={index} section={section} />
       ))}
-      
->>>>>>> ff9e596 (Add banners to explore product sections)
     </main>
   );
 };
