ad31e708696fc832c9c30a9f3a102c65be618d71
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-09-19 23:33:41 -0500

    Add advisory section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Banner from "./_components/homepage/Banner";
 import ExploreSection from "./_components/homepage/ExploreSection";
 import Colecciones from "./_components/Colecciones";
 import AboutArle from "./_components/homepage/AboutArle";
+import Button from "./_components/Button";
 
 const Home = async function () {
   const pageContent = await getHomepageContent();
@@ -27,7 +28,21 @@ const Home = async function () {
         <Colecciones colecciones={pageContent.colecciones} />
       </section>
       <AboutArle sobre={pageContent.sobre}/>
-      
+      <section className="bg-[#EDE7DE] px-4 py-6 flex flex-col gap-5">
+
+        <h3 className="text-zinc-800  text-center font-lora text-2xl font-semibold leading-[115%]">Eleva tu experiencia con nuestra asesoría</h3>
+        <ul className="pl-8 list-disc flex flex-col gap-4 text-zinc-800 text-base font-normal font-raleway leading-tight">
+          <li>Asesoramiento personalizado para encontrar el producto perfecto.</li>
+          
+          <li>Acceso a recomendaciones exclusivas y consejos de expertos.</li>
+
+          <li>Resuelve tus dudas y toma decisiones informadas.</li>
+
+          <li>Experimenta el lujo y la excelencia en cada interacción.</li>
+
+        </ul>
+      <Button className="bg-black px-4 py-2"><span className="text-stone-300">Quiero una asesoría</span></Button>
+      </section>
     </main>
   );
 };
