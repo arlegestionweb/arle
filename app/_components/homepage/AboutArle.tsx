d141bd2e7eb5f06675f6363dc2d7265057d6c858
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-09-20 22:45:31 -0500

    Add styles for About section on desktop version

--- app/_components/homepage/AboutArle.tsx
+++ app/_components/homepage/AboutArle.tsx
@@ -21,36 +21,32 @@ type AboutArleProps = {
 
 const AboutArle = ({ sobre }: AboutArleProps) => {
   return (
-
     <section className="relative w-screen min-h-[500px] bg-[#00002E] flex flex-col justify-center items-center md:px-10 md:flex-row md:py-12">
-      <div className="px-4 py-6">
-        <h3 className="text-white text-center text-2xl font-lora font-semibold leading-loose">
+      <section className="md:w-1/2 px-4 py-6 md:self-stretch">
+        <h3 className="text-white text-center md:text-start text-2xl md:text-[28px] font-lora font-semibold leading-loose">
           About Arlé
         </h3>
-        <div className=" text-zinc-100 text-base font-normal font-raleway leading-tight md:max-w-3xl">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar
-          elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit
-          amet consectetur adipiscing elit semper dalar elementum tempus hac
-          tellus libero accumsan.{" "}
-        </div>
-      </div>
-      
-      {/* //*TODO: Refactorizr componente Gradienimg */}
+        <p className=" text-zinc-100 text-base font-normal font-raleway leading-tight md:max-w-3xl">
+          Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet  consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. Lorem ipsum dolor sit amet consectetur   adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. {" "}
+        </p>
+      </section>
 
-      <GradientImage
-        alt={sobre.imagenes[0].alt}
-        src={sobre.imagenes[0].url}
-        layout="fill"
-        containerclassName="h-96 md:w-[400px] md:h-[300px] max-w-3xl"  
-      >
-        <div className="sticky px-4 py-6 w-full h-full flex items-end md:hidden">
-          <Button className="px-4 py-2 w-full bg-white justify-center items-center inline-flex">
-            <span className="text-zinc-800 text-lg font-medium font-inter leading-[27px]">
-              Listen our poadcast
-            </span>
-          </Button>
-        </div>        
-      </GradientImage>
+      {/* //*TODO: Refactorizr componente Gradienimg */}
+      <section className="w-[400px]">
+        <GradientImage
+          alt={sobre.imagenes[0].alt}
+          src={sobre.imagenes[0].url}
+          layout="fill"
+          containerclassName="h-[400px] bg-white self-stretch">
+          <div className="sticky px-4 py-6 w-full h-full flex items-end md:hidden">
+            <Button className="px-4 py-2 w-full bg-white justify-center items-center inline-flex">
+              <span className="text-zinc-800 text-lg font-medium font-inter leading-[27px]">
+                Listen our poadcast
+              </span>
+            </Button>
+          </div>
+        </GradientImage>
+      </section>
     </section>
   );
 };
