c8ca353d7d3d1d5ac890f1e23242605ba4fb9734
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-09-24 20:26:17 -0500

    Add scroll functionality to the banner

--- app/_components/homepage/Banner.tsx
+++ app/_components/homepage/Banner.tsx
@@ -25,29 +25,35 @@ const Banner = ({ banners }: BannerProps) => {
   };
 
   useEffect(() => {
-    const intervalId = setInterval(nextSlide, 5000); 
+    const intervalId = setInterval(nextSlide, 5000);
 
     return () => {
-      clearInterval(intervalId); 
+      clearInterval(intervalId);
     };
   }, [currentIndex]);
 
   return (
-    <section className="max-w-screen h-[70vh] pt-[70px] md:pt-0 relative group">
-      {/* //* image */}
-
-      <GradientImage
-        src={banners[currentIndex].imagen.url}
-        alt={banners[currentIndex].imagen.alt}
-        layout="fill"
-        imageClassName="fit object-cover object-top"
-        containerclassName="px-2 pt-2 pb-9 flex-col justify-end items-center gap-2.5 inline-flex">
-        <div className="sticky z-10 self-stretch h-[114px] flex-col justify-center items-center gap-2.5 flex">
-          <div className="self-stretch text-center text-white text-[32px] font-semibold font-['Lora'] uppercase leading-[38.40px]">
-            {banners[currentIndex].titulo}
-          </div>
-        </div>
-      </GradientImage>
+    <section className="max-w-screen h-[70vh] pt-[70px] md:pt-0 overflow-hidden relative group">
+  
+      <section className="banner-scrollbar flex w-full h-full overflow-x-scroll scroll-smooth snap-x snap-mandatory">
+        {banners.map((banner, index) => (
+          <>
+            <GradientImage
+              key={index + banner.titulo}
+              src={banner.imagen.url}
+              alt={banner.imagen.alt}
+              layout="fill"
+              imageClassName="fit object-cover object-top"
+              containerclassName={`snap-center snap-always ${index === 1 && 'snap-mandatory'} min-w-full px-2 pt-2 pb-9 flex-col justify-end items-center gap-2.5 inline-flex`}>
+              <div className="sticky z-10 self-stretch h-[114px] flex-col justify-center items-center gap-2.5 flex">
+                <div className="self-stretch text-center text-white text-[32px] font-semibold font-['Lora'] uppercase leading-[38.40px]">
+                  {banner.titulo}
+                </div>
+              </div>
+            </GradientImage>
+          </>
+        ))}
+      </section>
 
       <div className="flex absolute z-30 bottom-9 m-auto left-0 right-0 justify-center py-2">
         {banners.map((banner, index) => (
