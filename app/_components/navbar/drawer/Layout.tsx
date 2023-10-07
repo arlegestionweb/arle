bb3e7bb81e92c7695b8f71db48e7e562b70f835e
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-10-07 12:54:24 -0500

    Refactor menu component and add icons

--- app/_components/navbar/drawer/Layout.tsx
+++ app/_components/navbar/drawer/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiChevronLeft } from "react-icons/hi2";
 
 type LayoutProps = {
   volver?: () => void;
@@ -7,15 +8,16 @@ type LayoutProps = {
 
 const Layout = ({ volver, children }: LayoutProps) => {
   return (
-    <section className="fixed animate-slide-in-right top-0 h-screen w-80 bg-white">
+    <section className={"fixed animate-slide-in lg:animate-slide-in-right top-0 h-screen w-screen lg:w-80 bg-white shadow-2xl"}>
       <header className="p-4 h-16 flex items-center border-b border-stone-300">
         <h4
-          className="cursor-pointer text-black text-base font-medium font-inter leading-[21px]"
+          className="cursor-pointer text-black text-base font-medium font-inter leading-[21px] flex items-center gap-1"
           onClick={() => volver && volver()}>
+          <HiChevronLeft size={20}/>
           Volver
         </h4>
       </header>
-      <section className="px-4 flex flex-col gap-3">{children}</section>
+      <section className="px-4 flex flex-col">{children}</section>
     </section>
   );
 };
