49d112e2be5b46bea001ce07a03ac7f0a5ad0183
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-10-07 00:01:30 -0500

    Add initial animation to the menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,9 +31,21 @@ const config: Config = {
           '0%': { transform: 'translateX(-100%)' },
           '100%': { transform: 'translateX(0)' },
         },
+        // slide in from right
+        'slide-in-right': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        // opcaity in
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
       animation: {
         'slide-in': 'slide-in 0.2s ease-out',
+        'slide-in-right': 'slide-in-right 0.2s ease-out',
+        'fade-in': 'fade-in 0.2s ease-out',
       },
     },
   },
