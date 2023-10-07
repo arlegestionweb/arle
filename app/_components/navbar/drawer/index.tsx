70a8e192a84bee0a12de8e124dd239fb444afae9
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-10-07 03:09:20 -0500

    Implement logic for multi-level menu

--- app/_components/navbar/drawer/index.tsx
+++ app/_components/navbar/drawer/index.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/app/_lib/utils";
-import React from "react";
-import Level1 from "./Level1";
+import React, { useEffect, useState } from "react";
+import SearchInput from "../SearchInput";
+import Button from "../../Button";
+import Layout from "./Layout";
 
 type DrawerProps = {
   isOpen?: boolean;
@@ -8,7 +10,117 @@ type DrawerProps = {
   animation?: "slide-in" | "slide-in-right";
 };
 
+interface MenuItem {
+  label: string;
+  subMenu?: MenuItem[] | MenuItem;
+}
+
+const items: MenuItem = {
+  label: "Productos",
+  subMenu: [
+    {
+      label: "Perfumes",
+      subMenu: [
+        {
+          label: "Hombre",
+          subMenu: [
+            {
+              label: "Perfume Hombre",
+            },
+          ],
+        },
+        {
+          label: "Mujer",
+          subMenu: [
+            {
+              label: "Perfume Mujer",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Relojes",
+      subMenu: [
+        {
+          label: "Hombre",
+          subMenu: [
+            {
+              label: "Reloj Hombre",
+            },
+          ],
+        },
+        {
+          label: "Mujer",
+          subMenu: [
+            {
+              label: "Reloj Mujer",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Gafas",
+      subMenu: [
+        {
+          label: "Hombre",
+          subMenu: [
+            {
+              label: "Gafas Hombre",
+            },
+          ],
+        },
+        {
+          label: "Mujer",
+          subMenu: [
+            {
+              label: "Gafas Mujer",
+            },
+          ],
+        },
+      ],
+    },
+  ]
+};
+
 function Drawer({ isOpen, onClose, animation = "slide-in" }: DrawerProps) {
+  const [menu, setMenu] = useState<MenuItem>(items);
+  const [MenuStack, setMenuStack] = useState<MenuItem[]>([menu]);
+  const [currentLevel, setCurrentLevel] = useState<number>(0);
+
+  const handleItemClick = (item: MenuItem) => {
+    if (item.subMenu) {
+      setCurrentLevel( currentLevel + 1);
+
+      console.log('currentLevel', item);
+      
+      setMenuStack([...MenuStack, item]);
+      
+      setMenu(item);
+    } else {
+      // Si el elemento del menú no tiene submenú, puedes manejar la acción deseada aquí
+      // Por ejemplo, cerrar el menú o realizar alguna acción específica
+      console.log(`Hiciste clic en: ${item.label}`);
+    }
+    
+  };
+
+  const handleBack = () => {
+    // Regresar al menú anterior (nivel anterior)
+    if (currentLevel > 0 && MenuStack) {
+      setMenu( MenuStack[currentLevel - 1]);
+      setMenuStack(MenuStack.slice(0, currentLevel ));
+      setCurrentLevel(currentLevel - 1);
+    }
+    console.log('return to previous menu', MenuStack);
+    
+  };
+
+  // useEffect(() => {
+  //   console.log( {'menu': menu, 'previousMenu': MenuStack, 'currentLevel': currentLevel});
+  // }, [menu, MenuStack, currentLevel]);
+
   return (
     <div
       className="fixed z-40 w-screen bg-black bg-opacity-40 left-0 top-0 flex justify-end animate-fade-in"
@@ -24,11 +136,73 @@ function Drawer({ isOpen, onClose, animation = "slide-in" }: DrawerProps) {
             : " animate-slide-in"
         )}
         onClick={e => e.stopPropagation()}>
+        {/* Seccion inicial */}
 
-          <Level1/>
-
-
+        <section className="grow flex flex-col gap-3">
+          <header className="p-4 h-16 flex items-center border-b border-stone-300">
+            <h4 className="text-zinc-800 text-xl font-bold font-inter leading-normal">
+              Menú
+            </h4>
+          </header>
+          <section className="px-4 flex flex-col gap-3">
+            <div className="w-full ">
+              <SearchInput className="w-full" />
+            </div>
+            <section className="">
+              <h5 className="text-zinc-800 text-lg font-medium font-inter leading-snug">
+                Productos
+              </h5>
+              <ul>
+                {items.subMenu && (items.subMenu as MenuItem[]).map((item, index) => (
+                  <li
+                    key={index}
+                    className="cursor-pointer h-9 py-3 text-zinc-800 font-inter leading-tight"
+                    onClick={() => handleItemClick(item)}>
+                    {item.label}
+                  </li>
+                ))}
+              </ul>
+            </section>
+            <section className="py-3">
+              {/* //TODO: link */}
+              <p className="text-zinc-800 font-inter leading-tight">
+                Acerca de ARLÉ
+              </p>
+            </section>
+            {/* //TODO: Link */}
+            <Button className="text-black font-medium">
+              Pure White Cologne
+            </Button>
+          </section>
         </section>
+
+        {/* Levels  */}
+        {currentLevel > 0 && (
+          <Layout volver={handleBack}>
+            { MenuStack[1] && [...(MenuStack[1].subMenu as MenuItem[])].map((item, index) => (
+              <li
+                key={index}
+                className="cursor-pointer h-9 py-3 text-zinc-800 font-inter leading-tight"
+                onClick={() => handleItemClick(item)}>
+                {item.label}
+              </li>
+            ))}
+          </Layout>
+        )}
+
+        {currentLevel === 2 && (
+          <Layout volver={handleBack}>
+            {(menu?.subMenu as MenuItem[]).map((item, index) => (
+              <li
+                key={index}
+                className="cursor-pointer h-9 py-3 text-zinc-800 font-inter leading-tight"
+                onClick={() => handleItemClick(item)}>
+                {item.label}
+              </li>
+            ))}
+          </Layout>
+        )}
+      </section>
     </div>
   );
 }
