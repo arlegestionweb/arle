c57d1b0142f49d272fb246fb18b5e1f56d7dc994
Julian Bustos <julian.m.bustos@gmail.com>
2023-10-17 23:54:48 -0400

    ads the filterSection component with the first filter Item

--- app/listing/_components/Filters.tsx
+++ app/listing/_components/Filters.tsx
@@ -9,6 +9,7 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 import Drawer from "../../_components/Drawer";
 // import Drawer from "../Drawer";
 import { useSearchParams } from "next/navigation";
+import RedDot from "@/app/_components/RedDot";
 
 const Filters = ({
   areFiltersActive,
@@ -22,14 +23,14 @@ const Filters = ({
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const searchParams = useSearchParams();
   const allParams: { [key: string]: any } = {};
-  
+
   searchParams.forEach((value, param) => {
     if (!allParams[param]) {
       allParams[param] = [];
     }
     allParams[param].push(value);
   });
-  
+
   console.log("All Params:", allParams);
 
   // console.log("here", searchParams.values())
@@ -38,7 +39,7 @@ const Filters = ({
     setIsFilterOpen(!isFilterOpen);
   };
 
-  const filters = Object.keys(searchParams).map(key => {
+  const filters = Object.keys(searchParams).map((key) => {
     return key;
   });
 
@@ -49,7 +50,8 @@ const Filters = ({
           <div onClick={toggleFilter}>
             <Button
               className="flex items-center gap-2"
-              active={areFiltersActive}>
+              active={areFiltersActive}
+            >
               <FiFilter />
               Filtros
             </Button>
@@ -63,30 +65,12 @@ const Filters = ({
           searchParams={searchParams}
         /> */}
       </section>
-      <Drawer
-        isOpen={isFilterOpen}
-        onClose={toggleFilter}>
-        <div
-          className={`w-80 h-screen bg-color-bg-surface-1-default grid place-content-center relative`}>
-          <AiOutlineCloseCircle
-            onClick={toggleFilter}
-            className="cursor-pointer text-3xl absolute top-10 right-10"
-          />
-          {areFiltersActive && (
-            <Link
-              href="/listing"
-              onClick={toggleFilter}
-              className="text-3xl font-bold">
-              <Button>Quitar Filtros</Button>
-            </Link>
-          )}
-        </div>
-
-        <div
-          className="w-[10vw] h-screen"
-          onClick={toggleFilter}
-        />
-      </Drawer>
+      <FilterMenu
+        areFiltersActive={areFiltersActive}
+        isFilterOpen={isFilterOpen}
+        toggleFilter={toggleFilter}
+        searchParams={allParams}
+      />
     </>
   );
 };
@@ -96,35 +80,102 @@ type FilterMenuProps = {
   isFilterOpen: boolean;
   toggleFilter: () => void;
   areFiltersActive: boolean;
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 };
-const FilterMenu = ({isFilterOpen, toggleFilter, areFiltersActive}: FilterMenuProps) => {
+const FilterMenu = ({
+  isFilterOpen,
+  toggleFilter,
+  areFiltersActive,
+  searchParams,
+}: FilterMenuProps) => {
+  console.log({ searchParams });
   return (
-    <div
-      className={`${
-        isFilterOpen ? "w-screen" : "w-0"
-      } fixed z-10 top-[60px] left-0 transition-all h-screen bg-black bg-opacity-50 flex`}
-    >
-      <div
+    <Drawer isOpen={isFilterOpen} onClose={toggleFilter}>
+      <aside
         className={`${
           isFilterOpen ? "" : "hidden"
-        } w-[80vw] max-w-[400px] h-screen bg-white grid place-content-center relative`}
+        } w-[80vw] max-w-[400px] pt-16 h-screen bg-white grid relative`}
       >
         <AiOutlineCloseCircle
           onClick={toggleFilter}
-          className="cursor-pointer text-3xl absolute top-10 right-10"
+          className="cursor-pointer text-3xl absolute top-5 right-2"
         />
+        <FilterSection title="Línea" active>
+          <Checkbox
+            name="excelencia"
+            title="Excelencia"
+            description="La cima del Lujo, tu poder en cada detalle"
+          />
+          <Checkbox
+            name="elite"
+            title="Elite"
+            description="Calidad que inspira Liderazgo"
+          />
+        </FilterSection>
         {areFiltersActive && (
           <Link
             href="/listing"
             onClick={toggleFilter}
             className="text-3xl font-bold"
           >
-            <Button type="submit">Quitar Filtros</Button>
+            {/* <Button type="submit">Quitar Filtros</Button> */}
           </Link>
         )}
-      </div>
+      </aside>
       <div className="w-[10vw] h-screen" onClick={toggleFilter} />
+    </Drawer>
+  );
+};
+type CheckboxProps = {
+  name: string;
+  title: string;
+  description: string;
+};
+const Checkbox = ({ name, title, description }: CheckboxProps) => {
+  return (
+    <div className="flex gap-3">
+      <input type="checkbox" name={name} id="" />
+      <label htmlFor={name}>
+        <h4>{title}</h4>
+        <p className="text-xs">{description}</p>
+      </label>
     </div>
+  );
+};
+type FilterSectionProps = {
+  title: string;
+  children: React.ReactNode;
+  active?: boolean;
+};
+const FilterSection = ({
+  title,
+  children,
+  active = false,
+}: FilterSectionProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <section className="w-full h-fit px-4 py-6 border-b border-stone-300  flex-col justify-center items-start gap-3">
+      <div className="flex justify-between items-center w-full ">
+        <h3 className="font-bold relative">
+          {title}{" "}
+          {active ? (
+            <RedDot
+              // classNames="absolute -right-4 top-1/2 -translate-y-1/2"
+              position="centerRight"
+            />
+          ) : (
+            ""
+          )}
+        </h3>
+        <OpenCloseFilterButton
+          isOpen={isOpen}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && <>{children}</>}
+    </section>
   );
 };
 const BreadCrumbs = ({
@@ -143,7 +194,8 @@ const BreadCrumbs = ({
           key={filter}
           className={`${
             index >= 1 ? "ml-2" : ""
-          } flex items-center gap-2 text-sm`}>
+          } flex items-center gap-2 text-sm`}
+        >
           {!searchParams[filter] ? <></> : `${filter}:`}
           <Link href={`?${filter}=${searchParams[filter]}`}>
             {searchParams[filter]}
@@ -151,5 +203,51 @@ const BreadCrumbs = ({
         </li>
       ))}
     </ul>
+  );
+};
+
+type OpenCloseFilterButtonProps = {
+  isOpen: boolean;
+  onClick: () => void;
+};
+const OpenCloseFilterButton = ({
+  isOpen,
+  onClick,
+}: OpenCloseFilterButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-7 h-7 p-[7px] bg-neutral-100 grid place-content-center text-3xl relative"
+    >
+      {isOpen ? (
+        <Line />
+      ) : (
+        <>
+          <Line />
+          <Line
+            vertical
+            classNames=" absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2"
+          />
+        </>
+      )}
+    </button>
+  );
+};
+
+const Line = ({
+  classNames = "",
+  vertical = false,
+}: {
+  classNames?: string;
+  vertical?: boolean;
+}) => {
+  return (
+    <div
+      className={
+        (!vertical ? "h-[2px] w-3.5" : "w-[2px] h-3.5") +
+        " bg-neutral-600 " +
+        classNames
+      }
+    />
   );
 };
