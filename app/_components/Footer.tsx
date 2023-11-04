9124effb49b52ea96c9f3224fa706226837ff4e4
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-04 10:39:45 -0500

    Redesign the footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -8,18 +8,23 @@ import {
 } from "react-icons/fa";
 import { HiOutlineLocationMarker } from "react-icons/hi";
 
-
 const Footer = () => {
   return (
-    <section className="relative lg:pb-[420px]">
-      <footer className="lg:fixed bottom-0 -z-10 min-h-[420px] w-screen px-4 md:px-10 py-6 md:py-7 text-white bg-[#00002E]">
-        <section className="flex flex-col lg:flex-row gap-10 lg:gap-20 pb-8">
+    <section className="relative lg:pb-[100vh]">
+      <footer className=" flex flex-col lg:fixed bottom-0 -z-10 min-h-[100vh] w-screen px-4 md:px-10 pt-6 md:pt-20  text-white bg-[#00002E]">
+        <div className="flex-1 flex justify-center items-center">
+          <Image
+            src={"/logo-footer.svg"}
+            width={249}
+            height={176}
+            alt="logo"
+          />
+        </div>
+        <section className="flex flex-col items-start justify-center lg:flex-row gap-10 lg:gap-20 pb-[50px]">
           <div>
-            <Image src={"/logo-footer.png"} width={121} height={72} alt="logo" />
-            <p className="text-lg">Acerca de nosotros</p>
-          </div>
-          <div>
-            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-lora leading-7">Products</h3>
+            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-tajawal leading-7">
+              Products
+            </h3>
             <ul className="space-y-2">
               <li>Perfumes</li>
               <li>Relojes</li>
@@ -28,26 +33,48 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-lora leading-7">Conoce nuestras sedes</h3>
+            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-tajawal leading-7">
+              Arlé
+            </h3>
+            <ul className="space-y-2">
+              <li>Sobre Nosotros</li>
+              <li>Nuestras Sedes</li>
+              <li>Trabaja con Nosotros</li>
+            </ul>
+          </div>
+          <div>
+            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-tajawal leading-7">
+              Visítanos
+            </h3>
             <ul className="space-y-2">
               <li>
                 <div className="flex gap-1.5 items-center">
-                  <HiOutlineLocationMarker/>
-                  <h4 className="w-[268px] text-white font-bold font-inter leading-snug">[nombre-sede]</h4>
+                  <HiOutlineLocationMarker />
+                  <h4 className="w-[268px] text-white font-bold font-inter leading-snug">
+                    [nombre-sede]
+                  </h4>
                 </div>
-                <div className="w-72 text-white font-normal font-raleway leading-snug">Jardín plaza, Cra. 98 #16 - 200, Cali, Valle del Cauca</div>
+                <div className="w-72 text-white font-normal font-raleway leading-snug">
+                  Jardín plaza, Cra. 98 #16 - 200, Cali, Valle del Cauca
+                </div>
               </li>
               <li>
                 <div className="flex gap-1.5 items-center">
-                  <HiOutlineLocationMarker/>
-                  <h4 className="w-[268px] text-white font-bold font-inter leading-snug">[nombre-sede]</h4>
+                  <HiOutlineLocationMarker />
+                  <h4 className="w-[268px] text-white font-bold font-inter leading-snug">
+                    [nombre-sede]
+                  </h4>
                 </div>
-                <div className="w-72 text-white font-normal font-raleway leading-snug">Jardín plaza, Cra. 98 #16 - 200, Cali, Valle del Cauca</div>
+                <div className="w-72 text-white font-normal font-raleway leading-snug">
+                  Jardín plaza, Cra. 98 #16 - 200, Cali, Valle del Cauca
+                </div>
               </li>
             </ul>
           </div>
           <div>
-            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-lora leading-7">Legal</h3>
+            <h3 className="pb-4 text-stone-400 text-2xl font-semibold font-tajawal leading-7">
+              Legal
+            </h3>
             <ul className="space-y-2">
               <li>Términos y condiciones</li>
               <li>Política de privacidad</li>
@@ -58,27 +85,36 @@ const Footer = () => {
             </ul>
           </div>
         </section>
-        <section className="border-b-[1px] pb-3 border-white text-white flex justify-center space-x-5">
-          <a className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center" target="">
-            <FaFacebookF size={18}/>
+        <section className="border-b-[1px] pb-3 border-white text-white flex justify-center md:justify-end space-x-5">
+          <a
+            className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center"
+            target="">
+            <FaFacebookF size={18} />
           </a>
-          <a className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center" target="">
-            <FaTwitter size={18}/>
+          <a
+            className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center"
+            target="">
+            <FaTwitter size={18} />
           </a>
-          <a className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center" target="">
-            <FaInstagram size={18}/>
+          <a
+            className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center"
+            target="">
+            <FaInstagram size={18} />
           </a>
-          <a className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center" target="">
-            <FaLinkedinIn size={18}/>
+          <a
+            className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center"
+            target="">
+            <FaLinkedinIn size={18} />
           </a>
-          <a className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center" target="">
-            <FaYoutube size={18}/>
+          <a
+            className="w-9 h-9 flex bg-stone-400 text-black justify-center items-center"
+            target="">
+            <FaYoutube size={18} />
           </a>
         </section>
-        <section className="pt-3">
+        <section className="py-3">
           <div className="text-center text-stone-400 text-sm font-normal font-raleway leading-normal">
-            Copyright © 2023 Arlé | All Rights Reserved |<br />
-            Términos y condiciones | Privacy Policy
+            Copyright © 2023 Arlé | Todos los Derechos Reservados | Diseñado por GoJaguar.co
           </div>
         </section>
       </footer>
