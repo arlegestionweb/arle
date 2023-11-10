6f23bb3048f4292f4a77e73fe9412930b4f5aaf3
Julian Bustos <julian.m.bustos@gmail.com>
2023-11-10 14:50:24 -0500

    fixes the layout in the admin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,19 @@ import type { Metadata } from "next";
 import { Inter, Lora, Raleway, Tajawal } from "next/font/google";
 import Navbar from "./_components/navbar";
 import Footer from "./_components/Footer";
+import { headers } from "next/headers";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter'  });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
-const lora = Lora({ subsets: ["latin"], variable: '--font-lora' });
+const lora = Lora({ subsets: ["latin"], variable: "--font-lora" });
 
-const raleway = Raleway({ subsets: ["latin"], variable: '--font-raleway' });
+const raleway = Raleway({ subsets: ["latin"], variable: "--font-raleway" });
 
-const tajawal = Tajawal({weight: "400", subsets: ["latin"], variable: '--font-tajawal' });
+const tajawal = Tajawal({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-tajawal",
+});
 
 export const metadata: Metadata = {
   title: "Arlé",
@@ -24,12 +29,25 @@ export default function RootLayout({
 }) {
 
 
+  const headersList = headers();
+  const pathname = headersList.get("x-invoke-path");
+
+  if (pathname === "/admin") {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${lora.variable} ${raleway.variable} ${tajawal.variable} overflow-x-hidden`}>
-        <Navbar/>
+      <body
+        className={`${inter.variable} ${lora.variable} ${raleway.variable} ${tajawal.variable} overflow-x-hidden`}
+      >
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
