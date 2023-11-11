55425e3149d48c61c0fbd719e03d8c95d4a0873b
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-11 12:42:27 -0500

    Add temporary layout

--- app/listing/_components/Productos.tsx
+++ app/listing/_components/Productos.tsx
@@ -4,9 +4,9 @@ import { TProduct } from "@/sanity/queries/pages/listingQueries";
 
 const Productos = ({ productos }: { productos: TProduct[] }) => {
   return (
-    <div className="grid grid-cols-4 gap-y-6 gap-x-4">
+    <div className="grid grid-cols-[repeat(2,136px)] lg:grid-cols-[repeat(4,288px)] place-content-center gap-y-6 gap-x-4 ">
       {productos?.map((producto) => (
-        <Link href={`${producto.slug}`} key={producto.slug}>
+        <Link href={`${producto.slug}`} key={producto.slug} className="w-full justify-between flex flex-col gap-4">
           <ProductCard producto={producto} />
         </Link>
       ))}
