a4212a1b76c6ac8718255e52264eb26d1c25c884
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-12 17:33:16 -0500

    Add responsive styles to product layout

--- app/listing/_components/Productos.tsx
+++ app/listing/_components/Productos.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
 import ProductCard from "../../_components/listingsPage/ProductCard";
 import { TProduct } from "@/sanity/queries/pages/listingQueries";
-import { BannerType } from "@/app/_components/types";
 
 const Productos = ({ productos }: { productos: TProduct[]}) => {
   return (
-    <div className="grid grid-cols-[repeat(2,136px)] lg:grid-cols-[repeat(4,288px)] place-content-center gap-y-6 gap-x-4 ">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(4,minmax(128px,288px))] place-content-center gap-y-6 gap-x-4 ">
       {productos?.map((producto) => (
         // TODO make link
         // <Link href={`${producto.slug}`} 
