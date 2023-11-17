a408ce0b66483dc765bda8146e05ad2134a1d8f3
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-17 00:44:44 -0500

    Add products

--- app/_components/listingsPage/ProductCard.tsx
+++ app/_components/listingsPage/ProductCard.tsx
@@ -1,69 +1,126 @@
+"use client";
 import Image from "next/image";
 import {
   TProduct,
-  isGafa,
   isPerfume,
   isReloj,
 } from "@/sanity/queries/pages/listingQueries";
+import Button from "../Button";
+import { LuShoppingCart } from "react-icons/lu";
+import ProductSlide from "./ProductSlide";
+import Link from "next/link";
+import Labels, { LabelTypes } from "../Labels";
 
 const ProductoCard = ({ producto }: { producto: TProduct }) => {
+  // TODO: REFACTORIZAR, no repetir codigo
+  console.log(producto);
 
   if (isPerfume(producto)) {
-    // console.log("perfumes", { producto });
     return (
-      <div className="w-[136px] h-[279px] flex-col justify-start items-start gap-2 inline-flex">
-        <Image
+      <>
+        {producto.variantes[0].etiqueta && (
+          <Labels
+            labelType={producto.variantes[0].etiqueta as LabelTypes}
+            label={producto.variantes[0].etiqueta as LabelTypes}
+            className="left-1/2 z-[21] transform -translate-x-1/2 -translate-y-1/2"
+          />
+        )}
+        <CardLayout
+          link={producto.slug}
+          images={producto.imagenes}
           src={producto.imagenes[0].url}
           alt={producto.imagenes[0].alt || ""}
-          width={136}
-          height={136}
-          className="object-cover min-w-[136px]"
+          titulo={producto.titulo}
+          price={producto.marca}
         />
-        {producto.titulo}
-        <br />
-        {producto.marca}
-      </div>
+      </>
     );
   }
 
   if (isReloj(producto)) {
-    // console.log("relojes", { producto });
     return (
-      <div className="w-[136px] h-[279px] flex-col justify-start items-start gap-2 inline-flex">
-        <Image
+      <>
+        {producto.variantes[0].etiqueta && (
+          <Labels
+            labelType={producto.variantes[0].etiqueta as LabelTypes}
+            label={producto.variantes[0].etiqueta as LabelTypes}
+            className="left-1/2 z-[21] transform -translate-x-1/2 -translate-y-1/2"
+          />
+        )}
+        <CardLayout
+          link={producto.slug}
+          images={producto.variantes[0].imagenes}
           src={producto.variantes[0].imagenes[0].url}
           alt={producto.variantes[0].imagenes[0].alt || ""}
-          width={136}
-          height={136}
-          className="object-cover min-w-[136px]"
+          titulo={producto.modelo}
+          price={producto.marca}
         />
-        {producto.modelo}
-        <br />
-        {producto.marca}
-      </div>
+      </>
     );
   }
   // if (!producto.imagenes) return null;
 
-  if (isGafa(producto)) {
-    // producto
-    return (
-       <div className="w-[136px] h-[279px] flex-col justify-start items-start gap-2 inline-flex">
-          <Image
-            src={producto.variantes[0].imagenes[0].url}
-            alt={producto.variantes[0].imagenes[0].alt || ""}
-            width={136}
-            height={136}
-            className="object-cover min-w-[136px]"
+  return (
+    <div className="w-[136px] h-[279px] flex-col justify-start items-start gap-2 inline-flex">
+      gafas here
+    </div>
+  );
+};
+
+const CardLayout = ({
+  src,
+  alt,
+  titulo,
+  price,
+  link,
+  images,
+}: {
+  src: string;
+  alt: string;
+  titulo: string;
+  price: string;
+  link: string;
+  images: {
+    url: string;
+    alt?: string | null | undefined;
+  }[];
+}) => {
+  return (
+    <>
+      <section className="w-full  overflow-hidden">
+        {images.length > 1 ? (
+          <ProductSlide
+            slug={link}
+            imagesProduct={images}
+            className=" h-[180px] lg:h-[288px]"
           />
-          {producto.modelo}
-          <br />
-          {producto.marca}
-        </div>
-    );
+        ) : (
+          <Link href={link}>
+            <Image
+              src={src}
+              alt={alt}
+              width={288}
+              height={288}
+              className="object-cover h-[180px] w-full lg:h-[288px]"
+            />
+          </Link>
+        )}
+      </section>
 
-  }
-
+      <section className=" flex-1 justify-between font-tajawal flex flex-col gap-3">
+        <h3 className="text-xl font-bold leading-6 text-[#303030]">{titulo}</h3>
+        <p className="text-[18px] font-medium leading-5 text-[#4f4f4f]">
+          {price}
+        </p>
+      </section>
+      <Button className="bg-black text-[#CFCFCF] flex justify-center items-center gap-2">
+        <LuShoppingCart />
+        <span className="font-inter text-base font-medium leading-6">
+          Agregar
+        </span>
+      </Button>
+    </>
+  );
 };
 
 export default ProductoCard;
--- app/listing/_components/Productos.tsx
+++ app/listing/_components/Productos.tsx
@@ -1,14 +1,13 @@
-import Link from "next/link";
 import ProductCard from "../../_components/listingsPage/ProductCard";
 import { TProduct } from "@/sanity/queries/pages/listingQueries";
 
-const Productos = ({ productos }: { productos: TProduct[] }) => {
+const Productos = ({ productos }: { productos: TProduct[]}) => {
   return (
-    <div>
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(4,minmax(128px,288px))] place-content-center gap-y-6 gap-x-4 ">
       {productos?.map((producto) => (
-        <Link href={`${producto.slug}`} key={producto.slug}>
-          <ProductCard producto={producto} />
-        </Link>
+        <div key={producto.slug} className="relative pb-6 w-full justify-between flex flex-col gap-4">
+          <ProductCard producto={producto}/>
+        </div>
       ))}
     </div>
   );
