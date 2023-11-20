cf4e3128e9f400f2e0516ecd9440db35f6ddbe6b
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-19 20:34:54 -0500

    Change div to ul and li

--- app/listing/_components/Productos.tsx
+++ app/listing/_components/Productos.tsx
@@ -3,13 +3,13 @@ import { TProduct } from "@/sanity/queries/pages/listingQueries";
 
 const Productos = ({ productos }: { productos: TProduct[]}) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(4,minmax(128px,288px))] place-content-center gap-y-6 gap-x-4 ">
+    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(4,minmax(128px,288px))] place-content-center gap-y-6 gap-x-4 ">
       {productos?.map((producto) => (
-        <div key={producto.slug} className="relative pb-6 w-full justify-between flex flex-col gap-4">
+        <li key={producto.slug} className="relative pb-6 w-full justify-between flex flex-col gap-4">
           <ProductCard producto={producto}/>
-        </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
