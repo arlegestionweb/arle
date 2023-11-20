f5071d26e9e1d26ba2557ecd0d6031bbdd9f4c6a
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-19 20:26:25 -0500

    Correct type for "más vendido" labels

--- app/_components/Labels.tsx
+++ app/_components/Labels.tsx
@@ -9,7 +9,7 @@ const labelsVariants = cva(
     variants: {
       labelType: {
         nuevo: "bg-[#fff] border-[#bebebe]",
-        "mas vendido": "bg-[#e8f0e6] border-[#396231] text-[#274a21]",
+        "más vendido": "bg-[#e8f0e6] border-[#396231] text-[#274a21]",
         "pocas unidades": "bg-[#ffecc0] border-[#805a00] text-[#583e00]",
         agotado: "bg-[#fceae9] border-[#b52d2a] text-[#7d1f1d]",
         "súper descuento": "bg-[#bbfdd1] border-[#206e3a] text-[#164c28]",
@@ -23,14 +23,14 @@ const labelsVariants = cva(
 
 export type LabelTypes =
   | "nuevo"
-  | "mas vendido"
+  | "más vendido"
   | "pocas unidades"
   | "agotado"
   | "súper descuento"
 
 const icons = {
   "nuevo": <BsMagic />,
-  "mas vendido": <BsCheck2Circle />,
+  "más vendido": <BsCheck2Circle />,
   "pocas unidades": <CiCircleAlert />,
   "agotado": <CiCircleMinus size={19}/>,
   "súper descuento": <CiPercent />,
