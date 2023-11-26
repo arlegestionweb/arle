37376cb1684af8c0995ccdaaf3263d67634b2691
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-26 01:13:15 -0500

    Add variants to buttons

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,15 +1,33 @@
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 import RedDot from "./RedDot";
 import { cn } from "../_lib/utils";
-type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+import { VariantProps, cva } from "class-variance-authority";
+
+const buttonsVariants = cva(
+  "px-3 py-1.5 border border-black relative",
+  {
+    variants: {
+      labelType: {
+        light: "bg-white text-black",
+        dark: "bg-black text-[#CFCFCF]",
+      },
+    },
+    defaultVariants: {
+      labelType: "light",
+    },
+  }
+);
+
+type ButtonProps = ComponentPropsWithoutRef<"button"> & VariantProps<typeof buttonsVariants> & {
   children: ReactNode;
   onClick?: () => void;
   className?: string;
   active?: boolean;
 }
-const Button = ({children, active, className = ""}: ButtonProps) => {
+
+const Button = ({children, active, className = "", labelType}: ButtonProps) => {
   return (
-    <button className={cn(`px-3 py-1.5 border border-black relative`, className)}>
+    <button className={cn(buttonsVariants({labelType}), className)}>
       {children}
       {active && (
         <RedDot />
