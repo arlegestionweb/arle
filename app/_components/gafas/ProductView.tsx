de3922dd9186962ec64169bee2fcc2c4fc2386ea
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-27 00:10:34 -0500

    Add desktop product viewer component

--- app/_components/gafas/ProductView.tsx
+++ app/_components/gafas/ProductView.tsx
@@ -1,6 +1,8 @@
 import { TGafaPremium } from "@/sanity/queries/pages/types";
 import ProductSlide from "../ProductSlide";
 import { cn } from "@/app/_lib/utils";
+import React from "react";
+import Image from "next/image";
 
 type ProductViewerProps = {
   product: TGafaPremium;
@@ -10,21 +12,37 @@ type ProductViewerProps = {
 const ProductViewer = ({ product, className }: ProductViewerProps) => {
   return (
     <>
-      <div className=""></div>
       <ProductSlide
         slug={product.slug}
         imagesProduct={product.variantes[0].imagenes}
         className={cn("max-h-[377px] lg:hidden", className)}
       />
-      <ProductGrid product={product} className="hidden lg:block"/>
+      <ProductGrid
+        product={product}
+        className="hidden lg:block"
+      />
     </>
   );
 };
 
 const ProductGrid = ({ product, className }: ProductViewerProps) => {
+
   return (
-    <section className={cn("", className)}>
-      <>Hola</>
+    <section className={cn("col-span-6 px-5", className)}>
+      <div className="grid grid-cols-[repeat(2,minmax(200px,288px))] grid-rows-[repeat(3,377px)] h-[500px] w-full gap-[10px] justify-center">
+        {product.variantes[0].imagenes.map(image => (
+          <div
+            key={image.alt}
+            className="relative w-full h-full">
+            <Image
+              src={image.url}
+              alt={image.alt}
+              fill
+              className={`object-cover`}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
