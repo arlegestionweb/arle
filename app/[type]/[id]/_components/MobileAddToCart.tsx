aeee7775fcf55a77bf70466529473dcc678380ae
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-26 20:37:34 -0500

    Change colors in purchase buttons

--- app/[type]/[id]/_components/MobileAddToCart.tsx
+++ app/[type]/[id]/_components/MobileAddToCart.tsx
@@ -1,14 +1,15 @@
+import Button from "@/app/_components/Button";
 import { FastShoppingCartIcon, ShoppingCartIcon } from "./Icons";
 
 const MobileAddToCart = () => {
   return (
-    <div className="flex flex-col gap-2.5 sticky bottom-5 mx-auto items-center bg-color-bg-surface-1-default shadow py-2 px-2 my-2 w-fit">
-      <button className="flex items-center border w-full gap-2 bg-color-bg-surface-1-default px-3 py-2">
+    <div className="w-80 flex flex-col gap-2.5 sticky bottom-5 mx-auto items-center bg-color-bg-surface-1-default shadow py-2 px-2 my-2">
+      <Button className="flex justify-center items-center w-full gap-2 px-3 py-2">
         <ShoppingCartIcon /> Añadir al carrito
-      </button>
-      <button className="flex items-center border w-full gap-2 bg-color-bg-surface-1-default px-3 py-2">
+      </Button>
+      <Button labelType={"dark"} className="flex justify-center items-center w-full gap-2 px-3 py-2">
         <FastShoppingCartIcon /> Compra rápida
-      </button>
+      </Button>
     </div>
   );
 };
