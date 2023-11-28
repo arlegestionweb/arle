5a7c7b8813cedeab1d79378a4d1aa2ef7d859ba1
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-27 23:37:24 -0500

    Include MobileAddToCart component in desktop version

--- app/[type]/[id]/_components/MobileAddToCart.tsx
+++ app/[type]/[id]/_components/MobileAddToCart.tsx
@@ -1,9 +1,14 @@
 import Button from "@/app/_components/Button";
 import { FastShoppingCartIcon, ShoppingCartIcon } from "./Icons";
+import { cn } from "@/app/_lib/utils";
 
-const MobileAddToCart = () => {
+type PropsMobileAddToCart = {
+  className?:string
+}
+
+const MobileAddToCart = ({className}:PropsMobileAddToCart) => {
   return (
-    <div className="w-80 flex flex-col gap-2.5 sticky bottom-5 mx-auto items-center bg-color-bg-surface-1-default shadow py-2 px-2 my-2">
+    <div className={cn("w-80 flex flex-col gap-2.5 sticky bottom-5 mx-auto items-center bg-color-bg-surface-1-default shadow py-2 px-2 my-2", className)}>
       <Button className="flex justify-center items-center w-full gap-2 px-3 py-2">
         <ShoppingCartIcon /> Añadir al carrito
       </Button>
