c54e67622f6a5e6f7fbc95aa2d1f717f8de9f385
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-11-30 20:57:39 -0500

    Add scroll to ProductGrid component

--- app/_components/gafas/ProductView.tsx
+++ app/_components/gafas/ProductView.tsx
@@ -29,12 +29,17 @@ const ProductViewer = ({ product, className }: ProductViewerProps) => {
 const ProductGrid = ({ product, className }: ProductViewerProps) => {
 
   return (
-    <section className={cn("col-span-6 px-5", className)}>
-      <div className="grid grid-cols-[repeat(2,minmax(200px,288px))] grid-rows-[repeat(3,377px)] h-[500px] w-full gap-[10px] justify-center">
+    <section className={cn("relative col-span-6 px-5", className)}>
+      <div className="grid grid-cols-[repeat(2,minmax(200px,288px))] h-[1000px] overflow-y-scroll no-scrollbar overflow-hidden w-full gap-[10px] justify-center">
+
+        {product.variantes[0].imagenes.length > 5 && (
+          <div className="absolute right-[30px] top-[calc(1000px-85px)] w-[calc(576px+10px)] z-10 h-[85px] bg-gradient-to-b from-transparent to-[#00000080] opacity-70"></div>
+        )}
+
         {product.variantes[0].imagenes.map(image => (
           <div
             key={image.alt}
-            className="relative w-full h-full">
+            className="relative w-full h-[377px]">
             <Image
               src={image.url}
               alt={image.alt}
