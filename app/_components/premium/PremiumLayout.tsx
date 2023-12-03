8a05d5c6e10b3a6e3f1b21ef7ef31388866c9075
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-03 00:52:39 -0500

    Create layout for premium products page

---
+++ app/_components/premium/PremiumLayout.tsx
@@ -0,0 +1,73 @@
+import {
+  TGafaPremium,
+  TPerfumePremium,
+  TRelojPremium,
+  isPerfumePremium,
+} from "@/sanity/queries/pages/types";
+import ProductViewer from "./ProductView";
+import Labels, { LabelTypes } from "../Labels";
+import {
+  colombianPriceStringToNumber,
+  numberToColombianPriceString,
+} from "@/utils/helpers";
+
+type PremiumLayoutProps = {
+  product: TGafaPremium | TRelojPremium | TPerfumePremium;
+  children: JSX.Element[] | JSX.Element;
+};
+
+const PremiumLayout = ({ product, children }: PremiumLayoutProps) => {
+  const [modelo, codigo] = isPerfumePremium(product)
+    ? [product.titulo, product.variantes[0].codigoDeReferencia]
+    : [product.modelo, product.variantes[0].codigo];
+
+  return (
+    <section className="lg:grid lg:grid-cols-12 gap-8 min-h-screen row-auto w-full md:px-8 lg:max-w-[calc(1280px+32px)]">
+      <ProductViewer
+        product={product}
+        className="col-start-8 col-span-1"
+      />
+
+      <section className="col-span-6 col-start-1 row-start-1 flex flex-col px-5 lg:px-0 w-full relative">
+        <Labels
+          className="relative max-w-fit mt-4 mb-2"
+          labelType={product.variantes[0].etiqueta as LabelTypes}
+          label={product.variantes[0].etiqueta as LabelTypes}
+        />
+
+        <header>
+          <h1 className="text-zinc-800 text-[32px] font-normal font-kanit leading-10 w-full">
+            {product.marca} {modelo}
+          </h1>
+          <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
+            CODE: {codigo}
+          </span>
+          <p className="text-zinc-800 text-[32px] font-normal font-kanit leading-9">
+            ${product.variantes[0].precio}
+          </p>
+          <div className="text-justify">
+            <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
+              Págalo a 4 cuotas de $
+              {numberToColombianPriceString(
+                colombianPriceStringToNumber(product.variantes[0].precio) / 4
+              )}{" "}
+              sin intereses.
+              <br />
+              [provider]
+            </span>
+            <span className="text-neutral-600 text-sm font-normal font-tajawal leading-[16.80px]">
+              .{" "}
+            </span>
+            <span className="text-zinc-800 text-sm font-normal font-tajawal leading-[16.80px]">
+              Learn More
+            </span>
+          </div>{" "}
+        </header>
+
+        {children}
+      </section>
+    </section>
+  );
+};
+
+export default PremiumLayout;
