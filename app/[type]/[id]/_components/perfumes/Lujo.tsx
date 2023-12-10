38fc740a6e41ddab61a305baca60d02183150fc7
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-10 13:37:32 -0500

    Add components for information sections

--- app/[type]/[id]/_components/perfumes/Lujo.tsx
+++ app/[type]/[id]/_components/perfumes/Lujo.tsx
@@ -6,13 +6,15 @@ import {
   numberToColombianPriceString,
 } from "@/utils/helpers";
 import Cantidad from "../Cantidad";
+import InfoSection from "../InfoSection";
+import ProductSlide from "@/app/_components/ProductSlide";
 
 type TPerfumeLujoProps = {
   product: TPerfumeLujo;
 };
 
 const PerfumeLujo = ({ product }: TPerfumeLujoProps) => {
-  console.log(product);
+  console.log(product.banners);
 
   return (
     <section>
@@ -72,32 +74,135 @@ const PerfumeLujo = ({ product }: TPerfumeLujoProps) => {
           <Cantidad />
         </section>
 
-        <section className="bg-slate-900 min-h-[40vh] pt-6">
-          <section className="px-4 flex flex-col gap-2 pb-5">
-            <h2 className="text-color-bg-surface-1-default text-2xl font-normal font-crimson leading-7">
-              Descripción
-            </h2>
-            <p className="grow shrink basis-0 text-color-bg-surface-0-default text-lg font-normal font-tajawal leading-snug">
-              {product.descripcion.texto}
-            </p>
-          </section>
-          <div className="relative w-full h-64">
-            <Image
-              alt={product.descripcion.imagen.alt}
-              src={product.descripcion.imagen.url}
-              layout="fill"
-              className="object-cover"
+        <InfoSection
+          titulo="Descripción"
+          descripcion={product.descripcion.texto}
+          alt={product.descripcion.imagen.alt}
+          url={product.descripcion.imagen.url}
+        />
+
+        {product.inspiracion.usarInspiracion && (
+          <InfoSection
+            titulo="Inspiración"
+            descripcion={product.inspiracion.contenido!.resena || ""}
+            alt={product.inspiracion.contenido!.imagen?.alt || ""}
+            url={product.inspiracion.contenido!.imagen?.url || ""}
+            className="bg-color-bg-surface-1-default text-slate-900"
+          />
+        )}
+
+        <InfoSection
+          titulo="Inspiración"
+          DesciptionComp={<DetallesProducto />}
+          ImageComp={
+            <ProductSlide
+              imagesProduct={[
+                {
+                  alt: product.banners[0].imagen?.alt,
+                  url: product.banners[0].imagen?.url || "",
+                },
+              ]}
+              className="max-h-[377px]"
+              isLink={false}
             />
-          </div>
-        </section>
-
-        <section>
-          
-        </section>
-
+          }
+        />
       </section>
     </section>
   );
 };
 
 export default PerfumeLujo;
+
+const DetallesProducto = () => {
+  return (
+    <div className="w-80 px-4 pt-4 pb-5 border-b border-zinc-700 flex-col justify-start items-center gap-5 inline-flex">
+      <div className="self-stretch h-8 justify-start items-center inline-flex">
+        <div className="px-3 py-1 border-b-2 border-white flex-col justify-center items-center inline-flex">
+          <div className="justify-center items-center gap-2.5 inline-flex">
+            <div className="text-white text-base font-medium font-inter leading-normal">
+              Notas olfativas
+            </div>
+          </div>
+        </div>
+        <div className="px-3 py-1 border-b-2 border-zinc-700 flex-col justify-center items-center inline-flex">
+          <div className="justify-center items-center gap-2.5 inline-flex">
+            <div className="text-neutral-200 text-base font-medium font-inter leading-normal">
+              Ingredientes
+            </div>
+          </div>
+        </div>
+        <div className="px-3 py-1 border-b-2 border-zinc-700 flex-col justify-center items-center inline-flex">
+          <div className="justify-center items-center gap-2.5 inline-flex">
+            <div className="text-neutral-200 text-base font-medium font-inter leading-normal">
+              Info adicional
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="self-stretch h-[340px] flex-col justify-start items-center gap-5 flex">
+        <div className="self-stretch h-[52px] flex-col justify-center items-start gap-2 flex">
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+              Familia olfativa
+            </div>
+          </div>
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+              Amaderada: Cálida, elegante y natural.
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[52px] flex-col justify-center items-start gap-2 flex">
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-bold font-tajawal leading-snug">
+              Top Notes
+            </div>
+          </div>
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-normal font-tajawal leading-snug">
+              Bergamot, Pink Pepper, Cardamom, Heliotrope.
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[52px] flex-col justify-center items-start gap-2 flex">
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-bold font-tajawal leading-snug">
+              Heart Notes
+            </div>
+          </div>
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-normal font-tajawal leading-snug">
+              Jasmine, Lavender, Bitter almond, Geranium.
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[52px] flex-col justify-center items-start gap-2 flex">
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-bold font-tajawal leading-snug">
+              Base Notes
+            </div>
+          </div>
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-normal font-tajawal leading-snug">
+              Vanilla, Amber, Sandalwood, Natural Oud, Gaiac Wood
+            </div>
+          </div>
+        </div>
+        <div className="self-stretch h-[52px] flex-col justify-center items-start gap-2 flex">
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-bold font-tajawal leading-snug">
+              Best time to wear
+            </div>
+          </div>
+          <div className="self-stretch justify-start items-center gap-1 inline-flex">
+            <div className="grow shrink basis-0 text-white text-lg font-normal font-tajawal leading-snug">
+              During the colder months, or when going out on an adventure
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
