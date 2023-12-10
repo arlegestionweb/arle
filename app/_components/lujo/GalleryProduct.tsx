89538c8306579b0a3125e2952a84aa92622b9d8d
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-10 16:17:13 -0500

    Create thumbnail for GalleryProduct component

--- app/_components/lujo/GalleryProduct.tsx
+++ app/_components/lujo/GalleryProduct.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 type GalleryProductProps = {
   imagesProduct: {
@@ -10,15 +11,51 @@ type GalleryProductProps = {
 };
 
 const GalleryProduct = ({ imagesProduct }: GalleryProductProps) => {
+  const [index, setIndex] = useState(0);
+
+  const thumbnailElement = imagesProduct
+    .filter((img, idx) => {
+      if (idx < 6) {
+        return img;
+      }
+    })
+    .map((img, idx) => {
+      if ( imagesProduct.length > 1) {
+        return (
+          <section
+            key={`${img.url}-${idx}`}
+            className="relative w-14 h-14 more-images">
+            <Image
+              src={img.url}
+              alt={"vehicle"}
+              fill
+              style={{ objectFit: "cover" }}
+              onClick={() => setIndex(idx)}
+              className={idx === index ? "current" : ""}
+            />
+            <p
+              onClick={() => {
+                setIndex(idx);
+              }}>
+              +{imagesProduct.length - 6}
+            </p>
+          </section>
+        );
+      }
+    });
+
   return (
     <section className="relative max-h-[473px]">
       <Image
-        alt={imagesProduct[0].alt || ""}
-        src={imagesProduct[0].url}
+        alt={imagesProduct[index].alt || ""}
+        src={imagesProduct[index].url}
         width={300}
         height={473}
         className="object-cover w-full h-[473px]"
       />
+      <div>
+      {thumbnailElement}
+      </div>
     </section>
   );
 };
