de1ecf7c26173df206f0a5a3957c9602aeb81fb3
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-10 20:48:48 -0500

    Adapt thumbnail styles for desktop in GalleryProduct component

--- app/[type]/[id]/_components/perfumes/Lujo.tsx
+++ app/[type]/[id]/_components/perfumes/Lujo.tsx
@@ -18,72 +18,85 @@ const PerfumeLujo = ({ product }: TPerfumeLujoProps) => {
   console.log(product.imagenes);
 
   return (
-    <section>
-      {/* Product view */}
-      <GalleryProduct imagesProduct={product.imagenes} />
-
-      <section className="pb-6">
-        <Labels
-          className="mx-4 relative max-w-fit mt-4 mb-2"
-          labelType={"ultimas unidades"}
-          label={"ultimas unidades"}
+    <>
+      <section className="lg:grid lg:grid-cols-12 gap-8 min-h-screen row-auto w-full lg:max-w-[calc(1280px+32px)]">
+        {/* Product view */}
+        <GalleryProduct
+          className="col-start-1 col-span-6"
+          imagesProduct={product.imagenes}
         />
-        <header className="px-4 ">
-          <h1 className="text-zinc-800 text-[32px] font-normal font-kanit leading-10 w-full">
-            {product.marca}
-          </h1>
-          <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
-            CODE:
-          </span>
-          <p className="text-zinc-800 text-[32px] font-normal font-kanit leading-9">
-            ${product.variantes[0].precio}
-          </p>
-          <div className="text-justify">
-            <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
-              Págalo a 4 cuotas de $
-              {numberToColombianPriceString(
-                colombianPriceStringToNumber(product.variantes[0].precio) / 4
-              )}{" "}
-              sin intereses.
-              <br />
-              [provider]
-            </span>
-            <span className="text-neutral-600 text-sm font-normal font-tajawal leading-[16.80px]">
-              .{" "}
-            </span>
-            <span className="text-zinc-800 text-sm font-normal font-tajawal leading-[16.80px]">
-              Learn More
-            </span>
-          </div>{" "}
-        </header>
 
-        {/* <VariantSelector
+        <section className="pb-6 row-start-1 col-start-7 col-span-6 ">
+          <Labels
+            className="mx-4 relative max-w-fit mt-4 mb-2"
+            labelType={"ultimas unidades"}
+            label={"ultimas unidades"}
+          />
+          <header className="px-4 ">
+            <h1 className="text-zinc-800 text-[32px] font-normal font-kanit leading-10 w-full">
+              {product.marca}
+            </h1>
+            <h2>{product.marca} | {product.genero}</h2>
+            <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
+              CODE:{product.variantes[0].codigoDeReferencia}
+            </span>
+            <p className="text-zinc-800 text-[32px] font-normal font-kanit leading-9">
+              ${product.variantes[0].precio}
+            </p>
+            <div className="text-justify">
+              <span className="text-zinc-500 text-sm font-normal font-tajawal leading-[16.80px]">
+                Págalo a 4 cuotas de $
+                {numberToColombianPriceString(
+                  colombianPriceStringToNumber(product.variantes[0].precio) / 4
+                )}{" "}
+                sin intereses.
+                <br />
+                [provider]
+              </span>
+              <span className="text-neutral-600 text-sm font-normal font-tajawal leading-[16.80px]">
+                .{" "}
+              </span>
+              <span className="text-zinc-800 text-sm font-normal font-tajawal leading-[16.80px]">
+                Learn More
+              </span>
+            </div>{" "}
+          </header>
+
+          {/* <VariantSelector
           product={product}
           selectedVariant={selectedVariant}
           setSelectedVariant={setSelectedVariant}
         /> */}
 
-        <section className="px-4 mt-2 mb-6">
-          <Cantidad />
+          <section className="px-4 mt-2 mb-6">
+            <Cantidad />
+          </section>
         </section>
+      </section>
 
+      <div className="bg-slate-900 w-screen flex justify-center">
         <InfoSection
           titulo="Descripción"
           descripcion={product.descripcion.texto}
           alt={product.descripcion.imagen.alt}
           url={product.descripcion.imagen.url}
+          className="lg:max-w-[calc(1280px+32px)]"
         />
+      </div>
 
-        {product.inspiracion.usarInspiracion && (
+      {product.inspiracion.usarInspiracion && (
+        <div className="bg-color-bg-surface-1-default w-screen flex justify-center">
           <InfoSection
             titulo="Inspiración"
             descripcion={product.inspiracion.contenido!.resena || ""}
             alt={product.inspiracion.contenido!.imagen?.alt || ""}
             url={product.inspiracion.contenido!.imagen?.url || ""}
-            className="bg-color-bg-surface-1-default text-slate-900"
+            className="text-slate-900 lg:max-w-[calc(1280px+32px)]"
           />
-        )}
+        </div>
+      )}
 
+      <div className="bg-slate-900 w-screen flex justify-center">
         <InfoSection
           titulo="Inspiración"
           DesciptionComp={<DetallesProducto />}
@@ -95,13 +108,14 @@ const PerfumeLujo = ({ product }: TPerfumeLujoProps) => {
                   url: product.banners[0].imagen?.url || "",
                 },
               ]}
-              className="max-h-[377px]"
+              className="max-h-[377px] w-full"
               isLink={false}
             />
           }
+          className="w-full lg:max-w-[calc(1280px+32px)]"
         />
-      </section>
-    </section>
+      </div>
+    </>
   );
 };
 
