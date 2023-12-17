e259c9f7b344caf3fccdb74cd03c2288745baca1
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-16 22:59:25 -0500

    Add product details component

--- app/_components/lujo/DetallesProduct.tsx
+++ app/_components/lujo/DetallesProduct.tsx
@@ -1,50 +1,126 @@
-"use client"
+"use client";
+import { cn } from "@/app/_lib/utils";
 import { useState } from "react";
 
-const DetallesProducto = () => {
+type DetallesProductoProps = {
+  notasOlfativas?: {
+    notasDeBase: string[];
+    notasDeSalida: string[];
+    familiaOlfativa: string;
+    notasDeCorazon: string[];
+  };
+  ingredientes?: string[];
+  infoAdicional?: string;
+};
+
+const DetallesProducto = ({
+  notasOlfativas,
+  infoAdicional,
+  ingredientes,
+}: DetallesProductoProps) => {
   const [activeSection, setActiveSection] = useState("notasOlfativas");
 
-  const handleSectionChange = (section: any) => {
+  const handleSectionChange = (section: string) => {
     setActiveSection(section);
   };
 
   return (
     <section className="w-full h-full">
-      <nav className="pt-4 flex justify-center">
-        <h4
-          onClick={() => handleSectionChange("notasOlfativas")}
-          className={`px-4 py-2 border-b-2 ${
-            activeSection === "notasOlfativas" ? "border-white" : "border-zinc-700"
-          }`}
-        >
-          Notas olfativas
-        </h4>
-        <h4
-          onClick={() => handleSectionChange("ingredientes")}
-          className={`px-4 py-2 border-b-2 ${
-            activeSection === "ingredientes" ? "border-white" : "border-zinc-700"
-          }`}
-        >
-          Ingredientes
-        </h4>
-        <h4
-          onClick={() => handleSectionChange("infoAdicional")}
-          className={`px-4 py-2 border-b-2 ${
-            activeSection === "infoAdicional" ? "border-white" : "border-zinc-700"
-          }`}
-        >
-          Info adicional
-        </h4>
+      <nav className="no-scrollbar pt-4 flex md:justify-center justify-start overflow-x-auto overflow-y-hidden snap-x snap-mandatory w-full p-2">
+        {notasOlfativas && (
+          <h4
+            onClick={() => handleSectionChange("notasOlfativas")}
+            className={cn(
+              `px-4 py-2 border-b-2  whitespace-nowrap border-zinc-700`,
+              activeSection === "notasOlfativas" && "border-white"
+            )}>
+            Notas olfativas
+          </h4>
+        )}
+        {ingredientes && (
+          <h4
+            onClick={() => handleSectionChange("ingredientes")}
+            className={`px-4 py-2 border-b-2   whitespace-nowrap ${
+              activeSection === "ingredientes"
+                ? "border-white"
+                : "border-zinc-700"
+            }`}>
+            Ingredientes
+          </h4>
+        )}
+        {infoAdicional && (
+          <h4
+            onClick={() => handleSectionChange("infoAdicional")}
+            className={`px-4 py-2 border-b-2  whitespace-nowrap ${
+              activeSection === "infoAdicional"
+                ? "border-white"
+                : "border-zinc-700"
+            }`}>
+            Info adicional
+          </h4>
+        )}
       </nav>
 
-      <section style={{ display: activeSection === "notasOlfativas" ? "block" : "none" }}>
-        Contenido de Notas olfativas
-      </section>
-      <section style={{ display: activeSection === "ingredientes" ? "block" : "none" }}>
-        Contenido de Ingredientes
-      </section>
-      <section style={{ display: activeSection === "infoAdicional" ? "block" : "none" }}>
-        Contenido de Info adicional
+      <section className="mt-4">
+        {activeSection === "notasOlfativas" ? (
+          <>
+            {notasOlfativas && (
+              <section>
+                <div className="pb-5">
+                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+                    Notas De Base
+                  </h4>
+                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                    {notasOlfativas.notasDeBase.join(" ")}
+                  </p>
+                </div>
+                <div className="pb-5">
+                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+                    Notas De Salida
+                  </h4>
+                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                    {notasOlfativas.notasDeBase.join(" ")}
+                  </p>
+                </div>
+                <div className="pb-5">
+                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+                    Familia Olfativa
+                  </h4>
+                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                    {notasOlfativas.notasDeBase.join(" ")}
+                  </p>
+                </div>
+                <div className="pb-5">
+                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+                    Notas De Corazon
+                  </h4>
+                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                    {notasOlfativas.notasDeBase.join(" ")}
+                  </p>
+                </div>
+              </section>
+            )}
+          </>
+        ) : activeSection === "ingredientes" ? (
+          <>
+            {ingredientes &&
+              ingredientes.map(ingrediente => (
+                <p
+                  className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug"
+                  key={ingrediente}>
+                  {ingrediente}
+                </p>
+              ))}
+          </>
+        ) : (
+          <>
+            {infoAdicional && (
+              <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                {infoAdicional}
+              </p>
+            )}
+          </>
+        )}
       </section>
     </section>
   );
