276ec81a097b770632afcbe346c3e165fcb47c9c
Julian Bustos <julian.m.bustos@gmail.com>
2023-12-19 10:32:49 -0500

    Fix formatting issues in productPage queries

--- sanity/queries/pages/productPage/index.ts
+++ sanity/queries/pages/productPage/index.ts
@@ -37,7 +37,8 @@ const movimientoQuery = ` "movimiento": movimiento {
   }
 }`;
 
-const bannersQuery = `"banners": bannersDeProducto [] {
+const bannersQuery = `
+"banners": bannersDeProducto [] {
   imagenOVideo,
   "imagen": imagen {
     alt,
@@ -75,7 +76,14 @@ const variantesDeGafaQueryString = `
     }, 
   }
 `;
-export const productQuery: Record<TProductType, string> = {
+
+const garantiaQuery = `  
+  "garantia": garantia { 
+    meses, 
+    descripcion
+}`;
+
+const productQuery: Record<TProductType, string> = {
   relojesLujo: `{
     genero,
     mostrarCredito,
@@ -133,19 +141,17 @@ export const productQuery: Record<TProductType, string> = {
     },
     "inspiracion": ${inspiracionQuery},    
     modelo,
-    "garantia": garantia {
-      meses,
+    ${garantiaQuery},
+    ${movimientoQuery},
+    "caja": caja { 
+      diametro, 
+      "material": material -> nombre, 
+      "cristal": cristal -> titulo
     },
-   ${movimientoQuery},
-  "caja": caja { 
-    diametro, 
-    "material": material -> nombre, 
-    "cristal": cristal -> titulo
-  },
-  coleccionDeMarca,
-  descripcion,
-  ${bannersQuery},
-  "slug": slug.current,
+    coleccionDeMarca,
+    descripcion,
+    ${bannersQuery},
+    "slug": slug.current,
   }
 `,
   relojesPremium: `{
@@ -175,7 +181,7 @@ export const productQuery: Record<TProductType, string> = {
     _id,
     modelo,
     "slug": slug.current,
-    "garantia": garantia { meses, descripcion },
+    ${garantiaQuery},
     _type,
     "marca": marca->titulo,
     "detallesReloj": detallesReloj {
@@ -270,36 +276,15 @@ export const productQuery: Record<TProductType, string> = {
     coleccionDeMarca
   }`,
   gafasLujo: `{
-    ${bannersQuery},
-    mostrarCredito,
-    "especificaciones": especificaciones {
-      "tipoDeGafa": tipoDeGafa -> titulo,
-      "estiloDeGafa": estiloDeGafa -> titulo,
-      "lente": lente {
-        "tipo": tipo -> titulo,
-        "material": material -> titulo,
-      },
-      queIncluye,
-      "montura": montura {
-        "formaDeLaMontura": formaDeLaMontura -> titulo,
-        "materialMontura": materialMontura -> titulo,
-        "materialVarilla": materialVarilla -> titulo,
-      },
-      "paisDeOrigen": paisDeOrigen -> nombre
-    },
     _id,
-    descripcion,
-    "marca": marca->titulo,
     _type,
-    "garantia": garantia { 
-      meses, 
-      descripcion
-    },
-    "inspiracion": ${inspiracionQuery},    
-    "variantes": ${variantesDeGafaQueryString},
+    "marca": marca->titulo,
     modelo,
-    "slug": slug.current,
+    descripcion,
     genero,
+    mostrarCredito,
+    ${garantiaQuery},
+    "inspiracion": ${inspiracionQuery},    
     "detalles": detalles {
       usarDetalles,
       "contenido": contenido {
@@ -321,9 +306,56 @@ export const productQuery: Record<TProductType, string> = {
           }
         }
       },
-      descripcion,
     },
-    coleccionDeMarca
+    ${bannersQuery},
+    "especificaciones": especificaciones {
+      "paisDeOrigen": paisDeOrigen -> nombre,
+      queIncluye,
+      "tipoDeGafa": tipoDeGafa -> titulo,
+      "estiloDeGafa": estiloDeGafa -> titulo,
+      "montura": montura {
+        "formaDeLaMontura": formaDeLaMontura -> titulo,
+        "materialMontura": materialMontura -> titulo,
+        "materialVarilla": materialVarilla -> titulo,
+      },
+      "lente": lente {
+        "material": material -> titulo,
+        "tipo": tipo -> titulo,
+      },
+    },
+    "inspiracion": ${inspiracionQuery},    
+    "variantes": ${variantesDeGafaQueryString},
+    modelo,
+    "variantes": variantes [] {
+      "colorDeLaMontura": colorDeLaMontura -> {
+        nombre,
+        "color": color.hex
+      },
+      "colorDeLaVarilla": colorDeLaVarilla -> {
+        nombre,
+        "color": color.hex
+      },
+      "colorDelLente": colorDelLente -> {
+        nombre,
+        "color": color.hex
+      },
+      precio,
+      precioConDescuento,
+      etiqueta,
+      codigoDeReferencia,
+      registroInvima,
+      mostrarUnidadesDispobibles,
+      unidadesDisponibles,
+      "imagenes": imagenes[]{
+        alt,
+        "url": asset->url,
+      }, 
+    },
+    coleccionDeMarca,
+    "slug": slug.current,
+   
+   
+    
   }`,
   gafasPremium: `{
     _type,
@@ -347,10 +379,7 @@ export const productQuery: Record<TProductType, string> = {
         "materialVarilla": materialVarilla -> titulo,
       }
     },
-    "garantia": garantia { 
-      meses
-    },
-    coleccionDeMarca
+    ${garantiaQuery}
   }`,
 };
 
