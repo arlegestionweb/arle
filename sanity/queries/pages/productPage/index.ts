c70fda53e52193d46178cae17f5d7c3fbb440cef
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-23 16:29:26 -0500

    Resolve conflicts

--- sanity/queries/pages/productPage/index.ts
+++ sanity/queries/pages/productPage/index.ts
@@ -83,7 +83,7 @@ const garantiaQuery = `
     descripcion
 }`;
 
-const productQuery: Record<TProductType, string> = {
+export const productQuery: Record<TProductType, string> = {
   relojesLujo: `{
     genero,
     mostrarCredito,
