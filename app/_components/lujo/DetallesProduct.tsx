63bcd79d38492d37df8c505cf96f42518a80d514
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-12-25 21:30:00 -0500

    Refactor product details component

--- app/_components/lujo/DetallesProduct.tsx
+++ app/_components/lujo/DetallesProduct.tsx
@@ -1,24 +1,28 @@
 "use client";
-import { cn } from "@/app/_lib/utils";
-import { useState } from "react";
+import { cn, convertirCamelCaseATitulo } from "@/app/_lib/utils";
+import React, { useState } from "react";
 
 type DetallesProductoProps = {
-  notasOlfativas?: {
-    notasDeBase: string[];
-    notasDeSalida: string[];
-    familiaOlfativa: string;
-    notasDeCorazon: string[];
+  detalles?: {
+    [key: string]:
+      | string
+      | string[]
+      | {
+          [key: string]: string | string[];
+        };
   };
-  ingredientes?: string[];
-  infoAdicional?: string;
 };
 
 const DetallesProducto = ({
-  notasOlfativas,
-  infoAdicional,
-  ingredientes,
+  detalles,
 }: DetallesProductoProps) => {
-  const [activeSection, setActiveSection] = useState("notasOlfativas");
+  const [activeSection, setActiveSection] = useState<
+    | string
+    | string[]
+    | {
+        [key: string]: string | string[];
+      }
+  >("notasOlfativas");
 
   const handleSectionChange = (section: string) => {
     setActiveSection(section);
@@ -27,100 +31,69 @@ const DetallesProducto = ({
   return (
     <section className="w-full h-full">
       <nav className="no-scrollbar pt-4 flex md:justify-center justify-start overflow-x-auto overflow-y-hidden snap-x snap-mandatory w-full p-2">
-        {notasOlfativas && (
-          <h4
-            onClick={() => handleSectionChange("notasOlfativas")}
-            className={cn(
-              `px-4 py-2 border-b-2  whitespace-nowrap border-zinc-700`,
-              activeSection === "notasOlfativas" && "border-white"
-            )}>
-            Notas olfativas
-          </h4>
-        )}
-        {ingredientes && (
-          <h4
-            onClick={() => handleSectionChange("ingredientes")}
-            className={`px-4 py-2 border-b-2   whitespace-nowrap ${
-              activeSection === "ingredientes"
-                ? "border-white"
-                : "border-zinc-700"
-            }`}>
-            Ingredientes
-          </h4>
-        )}
-        {infoAdicional && (
-          <h4
-            onClick={() => handleSectionChange("infoAdicional")}
-            className={`px-4 py-2 border-b-2  whitespace-nowrap ${
-              activeSection === "infoAdicional"
-                ? "border-white"
-                : "border-zinc-700"
-            }`}>
-            Info adicional
-          </h4>
-        )}
+        {detalles &&
+          Object.keys(detalles).map(detalle => (
+            <>
+              <h4
+                onClick={() => handleSectionChange(detalle)}
+                className={cn(
+                  `px-4 py-2 border-b-2  whitespace-nowrap border-zinc-700`,
+                  activeSection === detalle && "border-white"
+                )}>
+                {convertirCamelCaseATitulo(detalle)}
+              </h4>
+            </>
+          ))}
       </nav>
 
       <section className="mt-4">
-        {activeSection === "notasOlfativas" ? (
-          <>
-            {notasOlfativas && (
-              <section>
-                <div className="pb-5">
-                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
-                    Notas De Base
-                  </h4>
-                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
-                    {notasOlfativas.notasDeBase.join(" ")}
-                  </p>
-                </div>
-                <div className="pb-5">
-                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
-                    Notas De Salida
-                  </h4>
-                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
-                    {notasOlfativas.notasDeBase.join(" ")}
-                  </p>
-                </div>
-                <div className="pb-5">
-                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
-                    Familia Olfativa
-                  </h4>
-                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
-                    {notasOlfativas.notasDeBase.join(" ")}
-                  </p>
-                </div>
-                <div className="pb-5">
-                  <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
-                    Notas De Corazon
-                  </h4>
-                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
-                    {notasOlfativas.notasDeBase.join(" ")}
-                  </p>
-                </div>
-              </section>
-            )}
-          </>
-        ) : activeSection === "ingredientes" ? (
-          <>
-            {ingredientes &&
-              ingredientes.map(ingrediente => (
-                <p
-                  className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug"
-                  key={ingrediente}>
-                  {ingrediente}
+        {detalles &&
+          Object.keys(detalles).map((detalle, index) => {
+            let arrDetalles: JSX.Element[] = [];
+
+            if (typeof detalles[detalle] === "string") {
+              arrDetalles.push(
+                <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                  {detalles[detalle] as string}
                 </p>
-              ))}
-          </>
-        ) : (
-          <>
-            {infoAdicional && (
-              <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
-                {infoAdicional}
-              </p>
-            )}
-          </>
-        )}
+              );
+            } else if (Array.isArray(detalles[detalle])) {
+              // Es de tipo string[]
+              arrDetalles.push(
+                <section>
+                  <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                    {(detalles[detalle] as string[]).join(", ")}
+                  </p>
+                </section>
+              );
+            } else {
+              // Es de tipo objeto
+              arrDetalles.push(
+                <>
+                  {Object.keys(detalles[detalle]).map(object => (
+                    <section key={index+object}>
+                      <div className="pb-5">
+                        <h4 className="w-72 text-neutral-200 text-lg font-bold font-tajawal leading-snug">
+                          {convertirCamelCaseATitulo(object)}
+                        </h4>
+                        <p className="w-72 text-zinc-100 text-lg font-normal font-tajawal leading-snug">
+                          {(detalles[detalle] as any)[object]}
+                        </p>
+                      </div>
+                    </section>
+                  ))}
+                </>
+              );
+            }
+
+            return (
+              <React.Fragment key={index}>
+                {arrDetalles.map(elementDetalle => (
+                  <>{detalle == activeSection && elementDetalle}</>
+                ))}
+              </React.Fragment>
+            );
+          })}
       </section>
     </section>
   );
