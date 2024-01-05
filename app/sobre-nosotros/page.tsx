6659ba669a59fa09e545853d5d62b3f3db3fc1d1
gojaguarco <gojaguarco@gmail.com>
2024-01-05 12:26:39 -0500

    about us finished

--- app/sobre-nosotros/page.tsx
+++ app/sobre-nosotros/page.tsx
@@ -19,7 +19,7 @@ const Page = async () => {
         {pageContent?.whyWeDoWhatWeDo?.imagenOVideo ?
           <video src={pageContent?.whyWeDoWhatWeDo?.video?.url}></video>
           :
-          <Image className=" filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.whyWeDoWhatWeDo?.imagen?.alt || ""} src={"https://cdn.sanity.io/images/qhszuxx1/production/a35b6e861900be80c2d11f1dda00c60693c1493e-326x369.png?fm=webp" || ""} />
+          <Image className=" filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.whyWeDoWhatWeDo?.imagen?.alt || ""} src={pageContent?.whyWeDoWhatWeDo?.imagen?.url || ""} />
         }
       </section>
       <section className="p-8 sm:py-11 sm:px-14 md:w-[50vw] md:py-10 md:pl-14 md:pr-14 max-w-3xl">
@@ -38,7 +38,7 @@ const Page = async () => {
         {pageContent?.whyUs?.imagenOVideo ?
           <video src={pageContent?.whyUs?.video?.url}></video>
           :
-          <Image className="filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.whyUs?.imagen?.alt || ""} src={"https://cdn.sanity.io/images/qhszuxx1/production/a35b6e861900be80c2d11f1dda00c60693c1493e-326x369.png?fm=webp" || ""} />
+          <Image className="filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.whyUs?.imagen?.alt || ""} src={pageContent?.whyUs?.imagen?.url || ""} />
         }
       </section>
     </section>
@@ -50,27 +50,39 @@ const Page = async () => {
         {pageContent?.howWeHelpOurClients?.imagenOVideo ?
           <video src={pageContent?.howWeHelpOurClients?.video?.url}></video>
           :
-          <Image className="filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.howWeHelpOurClients?.imagen?.alt || ""} src={"https://cdn.sanity.io/images/qhszuxx1/production/a35b6e861900be80c2d11f1dda00c60693c1493e-326x369.png?fm=webp" || ""} />
+          <Image className="filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.howWeHelpOurClients?.imagen?.alt || ""} src={pageContent?.howWeHelpOurClients?.imagen?.url || ""} />
         }
       </section>
       <section className="pl-24 md:pl-0 flex w-full max-w-3xl flex-col items-end md:items-start">
-        <h2>{pageContent?.howWeHelpOurClients?.titulo}</h2>
+        <h2 className="whitespace-pre-wrap">{pageContent?.howWeHelpOurClients?.titulo}</h2>
         <p className="text-right md:text-left">{pageContent?.howWeHelpOurClients?.descripcion}</p>
       </section>
     </section>
   }
-  {pageContent?.whyWeDoWhatWeDo?.on &&
+  {pageContent?.ourStory?.on &&
     <section className="md:h-[40vh] xl:h-[50vh] flex flex-col-reverse md:flex-row items-center justify-end bg-arle-blue text-color-bg-surface-1-default">
       <section className="p-8 sm:py-11 sm:px-14 md:w-[50vw] md:py-10 md:pl-14 md:pr-14 max-w-3xl">
-        <h2>{pageContent?.whyWeDoWhatWeDo?.titulo}</h2>
-        <p>{pageContent?.whyWeDoWhatWeDo?.descripcion}</p>
+        <h2>{pageContent?.ourStory?.titulo}</h2>
+        <p>{pageContent?.ourStory?.descripcion}</p>
       </section>
       <section className="h-[350px] flex relative md:h-full w-full md:w-[50vw] md:max-w-[50%]">
-        {pageContent?.whyWeDoWhatWeDo?.imagenOVideo ?
-          <video src={pageContent?.whyWeDoWhatWeDo?.video?.url}></video>
+        {pageContent?.ourStory?.imagenOVideo ?
+          <video src={pageContent?.ourStory?.video?.url}></video>
           :
-          <Image className=" filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.whyWeDoWhatWeDo?.imagen?.alt || ""} src={"https://cdn.sanity.io/images/qhszuxx1/production/a35b6e861900be80c2d11f1dda00c60693c1493e-326x369.png?fm=webp" || ""} />
+          <Image className=" filter brightness-[70%] w-full object-cover z-0" width={400} height={400} alt={pageContent?.ourStory?.imagen?.alt || ""} src={pageContent?.ourStory?.imagen?.url || ""} />
         }
+      </section>
+    </section>
+  }
+  {pageContent?.marcasAliadas &&
+    <section className="flex flex-col bg-white items-center gap-8 p-8 sm:py-11 sm:px-14 md:pt-10 md:pb-20">
+      <h2>Nuestras Marcas Aliadas</h2>
+      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(4,minmax(128px,288px))] place-items-center gap-y-6 gap-x-4 items-center justify-center">
+        {pageContent?.marcasAliadas?.map((item,i)=>(
+          <article key={i} className="w-40">
+            <Image width={200} height={200} alt={`marca ${item?.titulo} logo`} src={item?.logo?.url} />
+          </article>
+        ))}
       </section>
     </section>
   }
