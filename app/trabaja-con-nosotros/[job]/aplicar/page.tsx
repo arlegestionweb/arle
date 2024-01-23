76f740551447d46914b81b054ac6667e49ed6d3a
GoJaguar <gojaguarco@gmail.com>
2024-01-22 23:12:07 -0500

    aplicar delete

--- app/trabaja-con-nosotros/[job]/aplicar/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default () =>{
-    return (
-        <section className={`z-10 fixed top-[20%] w-[80%] h-[70%] bg-black`}>
-            <button >X</button>
-            Este es el modal          
-          </section> 
-    )
-}
