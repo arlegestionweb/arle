73b02705ff0e8fe84b485d5a8151e80e18b82dc4
Julian Bustos <julian.m.bustos@gmail.com>
2024-02-13 18:57:19 -0500

    Add NextRequest import and nextUrl property to GET route

--- app/success/[id]/test/route.ts
+++ app/success/[id]/test/route.ts
@@ -1,8 +1,11 @@
+import { NextRequest } from 'next/server';
+
 export const GET = async (
-  req: Request,
+  req: NextRequest,
 ) => { 
 
   return Response.json({
     url: req.url,
+    nextUrl: req.nextUrl,
   });
 };
