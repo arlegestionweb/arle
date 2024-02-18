bd148e29b9cf7f5e1f999f3e762dd45d76274958
Julian Bustos <julian.m.bustos@gmail.com>
2024-02-17 20:16:00 -0500

    Update AboutSubMenu styling

--- app/_components/AboutSubMenu.tsx
+++ app/_components/AboutSubMenu.tsx
@@ -23,16 +23,15 @@ const AboutSubMenu = () => {
 
 
   return (
-    <nav className="h-[53px] bg-white w-full">
+    <nav className="h-[53px] bg-white w-full overflow-x-scroll pl-8">
       <ul className="flex items-center gap-2 w-fit border-b-2 border-b-gray-400">
         {menuItems.map((item, index) => (
-          <li key={index} className={`mb-[-2px] ${pathname === item.href ? "border-b-2 border-black" : ""}`}>
+          <li key={index} className={`mb-[-2px] whitespace-nowrap ${pathname === item.href ? "border-b-2 border-black text-black" : "text-gray-400"}`}>
             <Link href={item.href}>
               {item.title}
             </Link>
           </li>
         ))}
-        {pathname}
       </ul>
 
     </nav>
