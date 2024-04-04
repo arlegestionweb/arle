e983786be3aff2a9c6c1fbf9c4224b9726696075
Julian Bustos <julian.m.bustos@gmail.com>
2024-04-04 14:12:59 -0400

    Update dependencies and configuration files

--- sanity/sanityAdmin.config.ts
+++ sanity/sanityAdmin.config.ts
@@ -1,21 +1,23 @@
 import { defineConfig } from "sanity";
 // import {visionTool} from "@sanity/vision"
-import {deskTool} from "sanity/desk"
-import structure from "./structure"
+import { deskTool } from "sanity/desk";
+import structure from "./structure";
 import { schemaTypes } from "./schemas/schemaTypes";
 import { colorInput } from "@sanity/color-input";
 
-
 export const sanityAdminConfig = {
   projectId: process.env.SANITY_PROJECT_ID || "",
-  dataset: "production",
+  dataset: process.env.SANITY_DATASET || "",
   useCdn: process.env.NODE_ENV === "production",
+  api: {
+    projectId: process.env.SANITY_PROJECT_ID || "",
+  },
   title: "Arlé Contenidos",
   apiVersion: "2021-10-21",
   basePath: "/admin",
   plugins: [
     deskTool({
-      structure
+      structure,
     }),
     colorInput(),
   ],
