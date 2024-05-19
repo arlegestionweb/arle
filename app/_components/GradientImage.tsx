45f270f39e7ce1c2ff6eb7cbdf98c773070f80d5
gojaguarco <gojaguarco@gmail.com>
2024-05-19 07:50:01 -0500

    gradient image optimization

--- app/_components/GradientImage.tsx
+++ app/_components/GradientImage.tsx
@@ -7,27 +7,52 @@ type GradientImageProps = {
   height?: number;
   width?: number;
   imageClassName?: string;
-  containerclassName?: string
+  containerclassName?: string;
   children?: React.ReactNode;
   gradientOff?: boolean;
   specialOpacity?: number;
   quality?: number;
 };
 
-function GradientImage({src, alt, layout, height, width, imageClassName, containerclassName, children, gradientOff, specialOpacity, quality}: GradientImageProps) {
+function GradientImage({
+  src,
+  alt,
+  layout,
+  height,
+  width,
+  imageClassName,
+  containerclassName,
+  children,
+  gradientOff,
+  specialOpacity,
+  quality,
+}: GradientImageProps) {
   return (
     <div className={`relative h-full w-full ${containerclassName}`}>
-      <img
-        src={src}
-        alt={alt}
-        width={width || 800}
-        height={height || 400}
-        // quality={quality || 80}
-        className={`object-cover absolute h-full w-full ${imageClassName}`}
-      />
+      <picture className={`object-cover absolute h-full w-full ${imageClassName}`}>
+        <source
+          sizes={`(max-width: 608px) ${width ? `${width}px` : `85vw`}, 608px`}
+          srcSet={`
+            ${src}?fit=max&q=${quality || 80}&w=1920&fm=webp 1920w,
+            ${src}?fit=max&q=${quality || 80}&w=1280&fm=webp 1280w,
+            ${src}?fit=max&q=${quality || 80}&w=640&fm=webp 640w,
+            ${src}?fit=max&q=${quality || 80}&w=320&fm=webp 320w,
+          `}
+          type="image/webp"
+        />
+        <img
+          src={src}
+          alt={alt || ""}
+          width={width || 800}
+          height={height || 400}
+          loading="lazy"
+          decoding="async"
+          className={`object-cover absolute h-full w-full ${imageClassName}`}
+        />
+      </picture>
       {/* Gradient */}
       {!gradientOff && (
-        <div className="w-full h-full left-0 bottom-0 absolute bg-gradient-to-t from-black to-transparent opacity-70" />
+        <div className={`w-full h-full left-0 bottom-0 absolute bg-gradient-to-t from-black to-transparent opacity-70`} />
       )}
 
       {/* content */}
--- app/_components/homepage/HeroBanner.tsx
+++ app/_components/homepage/HeroBanner.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 import { THeroSection } from "@/sanity/queries/pages/homepageQuery";
 
 type BannerProps = {
-    content: THeroSection,
-  className?: string,
+  content: THeroSection;
+  className?: string;
 };
 
 const Banner = ({ content, className }: BannerProps) => {
@@ -33,28 +33,32 @@ const Banner = ({ content, className }: BannerProps) => {
     }
   };
 
+  const width = 2000;
+  const height = null;
+
   return (
     <section
       className={cn(
         "max-w-screen h-[85vh] md:h-[75vh] overflow-hidden relative group",
         className
-      )}>
+      )}
+    >
       <section
         className="banner-scrollbar flex w-full h-full overflow-x-scroll scroll-smooth snap-x snap-mandatory"
         onScroll={handleScroll}
-        ref={bannerRef}>
+        ref={bannerRef}
+      >
         {content.banners.map((banner, index) => (
-          <React.Fragment key={index + banner.imagen.url
-          }>
+          <React.Fragment key={index + banner.imagen.url}>
             {banner.imagen && (
               <GradientImage
                 src={banner.imagen.url}
                 alt={banner.imagen.alt || ""}
                 layout="fill"
-                width={4000}
-                height={2000}
-                quality={100}
-                imageClassName="object-cover object-top"
+                width={2000}
+                height={1000}
+                quality={90}
+                imageClassName="object-center"
                 containerclassName={`snap-center snap-always ${
                   index === 1 && "snap-mandatory"
                 } min-w-full px-2 pt-2 flex-col justify-end items-center gap-2.5 inline-flex`}>
@@ -62,19 +66,22 @@ const Banner = ({ content, className }: BannerProps) => {
             )}
           </React.Fragment>
         ))}
-                <section className="absolute bottom-20 w-full z-10 flex justify-center default-paddings pointer-events-none">
-                  <section className="w-full flex flex-col justify-center items-start gap-4 max-w-screen-xl">
-                  <h1 className=" text-white lux-title text-2xl xs:text-3xl sm:text-4xl lg:text-[40px] max-w-sm sm:max-w-lg">
-                    {content.titulo}
-                  </h1>
-                  <h2 className=" text-white normal-subtitle">
-                    {content.subtitulo}
-                  </h2>
-                  {content.buttonText && (
-                  <Link className="light-button pointer-events-auto" href="/listing">{content.buttonText}</Link>
-                  )}
-                  </section>
-                </section>
+        <section className="absolute bottom-20 w-full z-10 flex justify-center default-paddings pointer-events-none">
+          <section className="w-full flex flex-col justify-center items-start gap-4 max-w-screen-xl">
+            <h1 className=" text-white lux-title text-2xl xs:text-3xl sm:text-4xl lg:text-[40px] max-w-sm sm:max-w-lg">
+              {content.titulo}
+            </h1>
+            <h2 className=" text-white normal-subtitle">{content.subtitulo}</h2>
+            {content.buttonText && (
+              <Link
+                className="light-button pointer-events-auto"
+                href="/listing"
+              >
+                {content.buttonText}
+              </Link>
+            )}
+          </section>
+        </section>
       </section>
 
       <div className="flex absolute z-20 bottom-9 m-auto left-0 right-0 justify-center py-2">
@@ -82,7 +89,9 @@ const Banner = ({ content, className }: BannerProps) => {
           <div
             key={index}
             className={`w-2.5 h-2.5 rounded-full mx-1.5 cursor-pointer ${
-              index === scrollPosition ? "bg-color-bg-surface-1-default" : "bg-[#8f8e94]"
+              index === scrollPosition
+                ? "bg-color-bg-surface-1-default"
+                : "bg-[#8f8e94]"
             }`}
             onClick={() => changeScrollPosition(index)}
           />
