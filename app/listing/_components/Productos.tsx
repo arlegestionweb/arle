99ae426afc424766983e543dc2271f2bec4e81db
Julian Bustos <julian.m.bustos@gmail.com>
2024-05-22 11:24:31 -0400

    adds basic pagination

--- app/listing/_components/Productos.tsx
+++ app/listing/_components/Productos.tsx
@@ -24,7 +24,7 @@ const Productos = ({ productos }: { productos: TProduct[] }) => {
     <>
       <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-[repeat(auto-fill,minmax(128px,288px))] place-content-center gap-y-6 gap-x-4 ">
         {prodsToRender?.map((producto, i) => (
-          <li key={`${producto._id}-${i}`} className="relative pb-6 w-full justify-between flex flex-col gap-1">
+          <li key={`${producto._id}-${i}`} className="relative pb-6 w-full justify-between flex flex-col gap-1 group">
             <ProductCard producto={producto} />
           </li>
         ))}
