047efb9d2bd0a7fd80731e7c94a77de8e81f07e1
gojaguarco <gojaguarco@gmail.com>
2024-06-12 11:43:05 -0500

    listo

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import ExploreSection from "./_components/homepage/ExploreSection";
 import Colecciones from "./_components/Colecciones";
 import AboutArle from "./_components/homepage/AboutArle";
 import Asesoria from "./_components/homepage/Asesoria";
-// import { unstable_noStore as noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
 const Home = async function () {
-  // noStore();
+  noStore();
   const pageContent = await getHomepageContent();
 
   if (!pageContent) return null;
