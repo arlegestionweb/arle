73e5faaffc70d7e7fe8e187b3ba68061696791de
gojaguarco <gojaguarco@gmail.com>
2024-06-14 08:32:15 -0500

    code url fix

--- app/_components/cart/CodigoDeDescuento.tsx
+++ app/_components/cart/CodigoDeDescuento.tsx
@@ -23,7 +23,7 @@ const CodigoDeDescuento = () => {
   };
 
   const verifyDiscountCode = async () => {
-    const response = await fetch("checkout/discount-code", {
+    const response = await fetch("/checkout/discount-code", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
