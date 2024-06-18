2b05da04ce5f1688b589111a1c5f01fe1685f91f
Julian Bustos <julian.m.bustos@gmail.com>
2024-06-18 05:53:07 -0400

    adds the transaction to the logs

--- app/transaccion-wompi/route.ts
+++ app/transaccion-wompi/route.ts
@@ -6,7 +6,7 @@ export const POST = async (req: Request, res: Response) => {
 
   const request = await req.json()
 
-  console.log({request})
+  console.log({request, transaction: request.data.transaction})
 
   return new Response("Transacción exitosa", {status: 200});
 
