51b56621967511ea2f7fe7567869f9078be43667
gojaguarco <gojaguarco@gmail.com>
2024-09-19 09:09:28 -0500

    whatsapp btn

--- app/_components/WhatsappButton.tsx
+++ app/_components/WhatsappButton.tsx
@@ -9,8 +9,7 @@ export const WhatsappButton = () => {
 
   return (
     <Link href="https://wa.me/573160700015?text=Hola,%20estoy%20interesad@%20en%20recibir%20una%20asesoría%20personalizada%20con%20ARLÉ!" target="_blank" className="fixed z-20 bottom-8 right-6 overflow-hidden w-[65px] h-[65px] cursor-pointer hover:scale-110 transition-all ease-out">
-        <img className="w-full h-full z-10 drop-shadow-[0_0_0.6px_rgb(0,0,0,0.8)] absolute" src="Whatsapp Negative.svg" alt="Whatsapp Logo" />
-        <img className="w-full h-full drop-shadow-[1px_1px_2px_rgb(0,0,0,0.4)] absolute" src="Whatsapp.svg" alt="Whatsapp Logo" />
+        <img className="w-full h-full drop-shadow-[1px_1px_2px_rgb(0,0,0,0.4)] absolute" src="/Whatsapp Logo.png" alt="Whatsapp Logo" />
     </Link>
   )
 }
