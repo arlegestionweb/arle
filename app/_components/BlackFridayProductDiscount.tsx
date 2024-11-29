d84b94a28721eb1a8e3e987292a63e145a03fbf5
gojaguarco <gojaguarco@gmail.com>
2024-11-29 15:40:34 -0500

    bf counter updated

--- app/_components/BlackFridayProductDiscount.tsx
+++ app/_components/BlackFridayProductDiscount.tsx
@@ -18,12 +18,11 @@ const BlackFridayProductDiscount = ({discountPercent}: {discountPercent: number}
         clearInterval(intervalId);
         setTimeRemaining('Discount has ended');
       } else {
-        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
         const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-        setTimeRemaining(`${days}días ${hours}h ${minutes}m ${seconds}s`);
+        setTimeRemaining(`${hours}h ${minutes}m ${seconds}s`);
       }
     }, 1000);
 
